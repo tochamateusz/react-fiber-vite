3263d855a4ecc756377713817c71fc04126b450c
toch <tocha.mateusz+main@gmail.com>
2023-11-16 10:40:23 +0100

    fix: clean styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,18 @@ import { Box } from "./App";
 import { Canvas } from "@react-three/fiber";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <Canvas>
-    <ambientLight />
-    <pointLight position={[10, 10, 10]} />
-    <Box position={[-1.2, 0, 0]} />
-    <Box position={[1.2, 0, 0]} />
-  </Canvas>,
+  <div
+    style={{
+      width: "50vw",
+      height: "75vh",
+      margin: "0 25vw",
+    }}
+  >
+    <Canvas flat linear>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  </div>,
 );
